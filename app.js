778ff372707208d778e16a9b39f2233f703290f8
Burgos Jorge <jorgeburgosjr69@gmail.com>
2021-06-13 06:41:45 -0400

    created array in app.js and commented the div in html

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const keyboard = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const reset = document.querySelector('btn__reset');
 let missed = 0;
+
+// Array
+
+const phrases = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday'
+];
