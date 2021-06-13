5ce9ac23ce3c7f0d0ec170d0f6f9b77a140cbe9f
Burgos Jorge <jorgeburgosjr69@gmail.com>
2021-06-13 06:32:56 -0400

    declared variables in app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+// Variables declaration
+
+const keyboard = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const reset = document.querySelector('btn__reset');
+let missed = 0;
